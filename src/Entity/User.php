5fb2d4223f7879be7098169606be0daea93730c6
MLBOULIGAND85 <mlbouligand@gmail.com>
2025-09-02 09:35:16 +0200

    MAJ gestion vue liste des évènements

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,7 +12,9 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Cet email existe déjà')]
+#[UniqueEntity(fields: ['telephone'], message: 'Ce numéro de téléphone existe déjà')]
+#[UniqueEntity(fields: ['pseudo'], message: 'Ce pseudo existe déjà')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
